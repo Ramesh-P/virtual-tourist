b1251641f5a0fbed0dc6f25736f85890ce217738
Ramesh Parthasarathy <RameshP@rameshs-imac.home>
2017-02-22 22:23:55 -0500

    style: Add custom fonts

--- Virtual Tourist/ViewController.swift
+++ Virtual Tourist/ViewController.swift
@@ -11,15 +11,24 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Verify available fonts
+        /*
+        for family: String in UIFont.familyNames {
+            print("\(family)")
+            
+            for names: String in UIFont.fontNames(forFamilyName: family) {
+                print("== \(names)")
+            }
+        }
+ */
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
