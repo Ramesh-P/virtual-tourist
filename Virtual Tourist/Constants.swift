34d2de01d0fb13e78dcbb658b55b9702fb043fee
Ramesh Parthasarathy <RameshP@rameshs-imac.home>
2017-02-24 16:08:31 -0500

    feat: Add travel locations class functions
    - Overlay and render tile to map view

--- Virtual Tourist/Constants.swift
+++ Virtual Tourist/Constants.swift
@@ -12,11 +12,20 @@ import UIKit
 // MARK: Constants
 struct Constants {
     
-    // MARK: ScreenHeight
+    // MARK: Screen Height
     struct ScreenHeight {
         static let phoneSE: CGFloat = 568.0
         static let phone: CGFloat = 667.0
         static let phonePlus: CGFloat = 736.0
+    }
+    
+    // MARK: Map Overlay
+    struct MapOverlay {
+        static let openStreetMapCarto = "http://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        static let openStreetMapGrayscale = "https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+        static let cartoLightAll = "http://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
+        static let cartoLightNolabels = "http://a.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png"
+        static let stamenToner = "http://a.tile.stamen.com/toner/{z}/{x}/{y}.png"
     }
 }
 
--- Virtual Tourist/TravelLocationsMapViewController.swift
+++ Virtual Tourist/TravelLocationsMapViewController.swift
@@ -6,6 +6,32 @@
 //  Copyright © 2017 Ramesh Parthasarathy. All rights reserved.
 //
 
+/**
+ * Credits/Attributions:
+ * Map tiles are property of OpenStreetMap Foundation and their contributors.
+ *
+ * OpenStreetMap Attribution
+ * -------------------------
+ * OpenStreetMap® is open data, licensed under the Open Data Commons Open Database License (ODbL) by the OpenStreetMap Foundation (OSMF).
+ * https://www.openstreetmap.org/copyright
+ *
+ * CARTO Attribution
+ * -----------------
+ * https://carto.com/attribution
+ *
+ * Stamen Attribution
+ * ------------------
+ * Except otherwise noted, each of these map tile sets are © Stamen Design, under a Creative Commons Attribution (CC BY 3.0) license.
+ * http://maps.stamen.com/#terrain/12/37.7706/-122.3782
+ *
+ * For Toner and Terrain: Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.
+ * Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href=“http://www.openstreetmap.org/copyright">ODbL</a>.
+ *
+ * For Watercolor: Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.
+ * Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.
+ *
+ */
+
 import Foundation
 import UIKit
 import MapKit
@@ -44,22 +70,38 @@ class TravelLocationsMapViewController: UIViewController, UINavigationController
         // Initialize
         navigationControllerDelegate.setAppTitleImage(self)
         banner.image = UIImage(named: appDelegate.bannerImage)
-        
-        // Verify available fonts
-        /*
-        for family: String in UIFont.familyNames {
-            print("\(family)")
-            
-            for names: String in UIFont.fontNames(forFamilyName: family) {
-                print("== \(names)")
-            }
-        }
- */
+        setMapTileOverlay()
 }
 
     override func didReceiveMemoryWarning() {
         
         super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: Class Functions
+    func setMapTileOverlay() {
+        
+        // Add overlay tiles to map view
+        let template = Constants.MapOverlay.cartoLightAll
+        let overlay = MKTileOverlay(urlTemplate: template)
+        
+        overlay.canReplaceMapContent = true
+        travelLocations.add(overlay, level: .aboveLabels)
+        travelLocations.delegate = self
+    }
+}
+
+// MARK: TravelLocationsMapViewController+MKMapViewDelegate
+extension TravelLocationsMapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        // Render overlay tiles on the map view
+        guard let tileOverlay = overlay as? MKTileOverlay else {
+            return MKOverlayRenderer()
+        }
+        
+        return MKTileOverlayRenderer(tileOverlay: tileOverlay)
     }
 }
 
