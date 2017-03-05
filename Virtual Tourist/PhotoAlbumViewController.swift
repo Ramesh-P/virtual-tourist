6c198148dffa270e7894751f155d3606ef76a1f7
Ramesh Parthasarathy <RameshP@rameshs-imac.home>
2017-03-05 18:19:07 -0500

    feat: Add storyboard view controllers
    - Add photo album view controller
    - Add navigation controller
    - Associate photo album class & view controller

---
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoAlbumViewController.swift
+//  Virtual Tourist
+//
+//  Created by Ramesh Parthasarathy on 3/5/17.
+//  Copyright © 2017 Ramesh Parthasarathy. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
