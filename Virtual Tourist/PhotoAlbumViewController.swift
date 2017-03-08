5b9f4da24cc97f87b30114eddce050e993451c90
Ramesh Parthasarathy <RameshP@rameshs-imac.home>
2017-03-07 19:08:53 -0500

    feat: Configure photo album view controller
    - Add outlets and actions to control objects

--- Virtual Tourist/PhotoAlbumViewController.swift
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -11,13 +11,37 @@ import UIKit
 class PhotoAlbumViewController: UIViewController {
     
     // MARK: Properties
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     var pin: Pin?
-
-
+    
+    // MARK: Outlets
+    @IBOutlet weak var album: UICollectionView!
+    @IBOutlet weak var banner: UIImageView!
+    @IBOutlet weak var hint: UILabel!
+    @IBOutlet weak var photoAction: UIBarButtonItem!
+    @IBOutlet weak var addNewPhotos: UIBarButtonItem!
+    @IBOutlet weak var deletePhotos: UIBarButtonItem!
+    
+    // MARK: Actions
+    @IBAction func editPhotos(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func addNewPhotoCollection(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func deleteSelectedPhotos(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    // MARK: Overrides
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
+        // Initialize
+        banner.image = UIImage(named: appDelegate.bannerImage)
     }
 
     override func didReceiveMemoryWarning() {
