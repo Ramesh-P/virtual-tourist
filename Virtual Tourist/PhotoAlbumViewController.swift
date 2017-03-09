806761c9537f92bb6df2b253a6c33c3100deb824
Ramesh Parthasarathy <RameshP@rameshs-imac.home>
2017-03-09 10:17:05 -0500

    feat: Add photo album class functions
    - Fetch photos from data store and display

--- Virtual Tourist/PhotoAlbumViewController.swift
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -16,7 +16,11 @@ class PhotoAlbumViewController: UIViewController {
     
     // MARK: Properties
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>?
+    var insertIndexPath: [NSIndexPath]?
+    var deleteIndexPath: [NSIndexPath]?
     var pin: Pin?
+    var photoCollection = [Photo]()
     var address: String = String()
     var isEditingPhotos: Bool = Bool()
     
@@ -63,10 +67,21 @@ class PhotoAlbumViewController: UIViewController {
         // Initialize
         isEditingPhotos = false
         banner.image = UIImage(named: appDelegate.bannerImage)
+        fetchedResultsController?.delegate = self
         
         // Layout
         getGeoLocation()
         setActions()
+        initializeLayout()
+        fetchPhotos()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        // Layout
+        album.reloadData()
     }
     
     override func didReceiveMemoryWarning() {
@@ -116,6 +131,17 @@ class PhotoAlbumViewController: UIViewController {
             hint.text = (address != "") ? address : "Unknown Location"
         }
     }
+    
+    func initializeLayout() {
+        
+        // Layout cells
+        let itemsPerRow: CGFloat = 3.0
+        var size: CGFloat = 0.0
+        let flowLayout = album.collectionViewLayout as? UICollectionViewFlowLayout
+        
+        size = UIScreen.main.bounds.size.width / itemsPerRow
+        flowLayout?.itemSize = CGSize(width: size, height: size + 64)
+    }
 }
 
 // MARK: PhotoAlbumViewController+FetchedResultsControllerDelegate
@@ -130,6 +156,50 @@ extension PhotoAlbumViewController: NSFetchedResultsControllerDelegate {
         
         return NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: appDelegate.stack.context, sectionNameKeyPath: nil, cacheName: nil)
     }
+    
+    //func fetchPhotos() -> [Photo] {
+    func fetchPhotos() {
+        
+        // Fetch location photos from data store
+        fetchedResultsController = fetchedResults()
+        
+        do {
+            try fetchedResultsController?.performFetch()
+            if let results = fetchedResultsController?.fetchedObjects as? [Photo] {
+                photoCollection = results
+            }
+        } catch {
+            fatalError("Could not fetch photos: \(error)")
+        }
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        
+        // Reset index
+        insertIndexPath = [NSIndexPath]()
+        deleteIndexPath = [NSIndexPath]()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        
+        // Perform batch changes
+        album.performBatchUpdates( {
+            self.album.insertItems(at: self.insertIndexPath as! [IndexPath])
+            self.album.deleteItems(at: self.deleteIndexPath as! [IndexPath])
+        }, completion: nil)
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            insertIndexPath?.append(newIndexPath! as NSIndexPath)
+        case .delete:
+            deleteIndexPath?.append(indexPath! as NSIndexPath)
+        default:
+            break
+        }
+    }
 }
 
 // MARK: PhotoAlbumViewController+CollectionViewDataSource+CollectionViewDelegate
@@ -138,12 +208,36 @@ extension PhotoAlbumViewController: UICollectionViewDataSource, UICollectionView
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         return 21
+        
+        /*
+        if let fetchedResultsController = fetchedResultsController {
+            return fetchedResultsController.sections![section].numberOfObjects
+        } else {
+            return 0
+        }
+ */
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! Cell
+        let picture = fetchedResultsController?.object(at: indexPath) as! Photo
+        
+        if (picture.image == nil) {
+            
+        } else {
+            cell.thumbnail.image = UIImage(data: picture.image as! Data)
+            cell.title.text = picture.title
+        }
+        
         return cell
     }
+}
+
+// MARK: PhotoAlbumViewController+Extension+Photos
+extension PhotoAlbumViewController {
+    
+    // MARK: Photos
+    
 }
 
